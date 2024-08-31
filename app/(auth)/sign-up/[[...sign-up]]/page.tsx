390f84408e4d93e081db1f5c1cf71f37cfbcbbe8
Delusional-bishop <you@example.com>
2024-09-01 05:08:59 +0530

    last commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SignUnPage = () => {
+  return (
+    <main className='auth-page'>
+        <SignUp/>
+    </main>
+  )
+}
+
+export default SignUnPage
